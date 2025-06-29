0d3ddb51658a96eb2c2d7d8046dc4a5a23e41605
Gautham <gauthamskaniyattil@gmail.com>
2025-06-29 14:07:22 +0530

    Added Delete notes controller

--- controllers/notesController.js
+++ controllers/notesController.js
@@ -42,7 +42,22 @@ const postNote = async (req, res) => {
     return res.status(500).json({ msg: "Failed to Insert Note" });
   }
   res.status(201).json({ msg: "Note inserted sucessfully" });
-  console.log(`ID : ${note.ObjectId}`);
+  console.log(`ID : ${note._id}`);
 };
 
-export { getAllNotes, getNote, postNote };
+//@desc Method to Delete specific Notes
+//@Route DELETE /api/posts/:id
+
+const deleteNote = async (req, res) => {
+  const id = req.params.id;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json({ msg: "Error : Not a valid MongoDB ID" });
+  }
+  const note = notesModel.findOneAndDelete({ _id: id });
+  if (!note) {
+    return res.status(404).json({ msg: "No Notes found of that ID" });
+  }
+  res.status(200).json({ msg: "note deleted Successfully!" });
+};
+
+export { getAllNotes, getNote, postNote, deleteNote };
