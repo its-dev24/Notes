7883df3ce771829373644f16e093bbd9740342d8
Gautham <gauthamskaniyattil@gmail.com>
2025-06-29 12:20:24 +0530

    Controller : getNote
    added the controller function to get a specific note

--- controllers/notesController.js
+++ controllers/notesController.js
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import notesModel from "../model/notesModel.js";
 
 // @desc Method to get all Notes
@@ -9,6 +10,21 @@ const getAllNotes = async (req, res) => {
     res.status(404).json({ msg: "No Notes Found" });
   }
   res.status(200).json(notesData);
+};
+
+//@desc Method to get specific Notes
+//@Route GET /api/posts/:id
+
+const getNote = async (req, res) => {
+  const id = req.params.id;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json({ msg: "Invalid MongoDB ID" });
+  }
+  const noteData = await notesModel.findById(id);
+  if (!noteData) {
+    return res.status(404).json("No Notes Found");
+  }
+  res.status(200).json(noteData);
 };
 
 export { getAllNotes };
